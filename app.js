36e4a2ef298277d8dd193654e046533f2e58de01
Ubuntu <ubuntu@ip-172-31-25-113.ap-southeast-1.compute.internal>
2024-08-13 16:34:28 +0000

    Initial commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
